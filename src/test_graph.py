ad378438c252603111c2ffb311abe556609060c2
Diadji <papediadji123@gmail.com>
2026-08-10 15:59:37 +0200

    add new node

--- src/test_graph.py
+++ src/test_graph.py
@@ -6,15 +6,24 @@ class State(TypedDict):
 
 def node_a(state: State):
     return {"message": state["message"] + " → passé par A"}
-3632
+
 def node_b(state: State):
     return {"message": state["message"] + " → passé par B"}
+
+
+def node_c(state: State):
+    return {"message":state["message"]  + " -> passé par C"}
+
 
 graph = StateGraph(State)
 graph.add_node("A", node_a)
 graph.add_node("B", node_b)
+
+graph.add_node("C", node_c)
 graph.add_edge(START, "A")
-graph.add_edge("A", "B")
+graph.add_edge("A", "C")
+
+graph.add_edge("C", "B")
 graph.add_edge("B", END)
 
 app = graph.compile()
